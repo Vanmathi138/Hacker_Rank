4ae480108d846332f6d71b71fbb8512fe6b95af0
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-08-20 16:52:28 +0530

    first commit

---
+++ src/Algorithms/BirthdayCakeCandles.java
@@ -0,0 +1,33 @@
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BirthdayCakeCandles {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>(4);
+        arr.add(4);
+        arr.add(2);
+        arr.add(4);
+        arr.add(3);
+        int result = solution(arr);
+        System.out.println(result);
+    }
+
+    private static int solution(List<Integer> arr) {
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+
+        // Find the maximum height of the candles
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) > max) {
+                max = arr.get(i);
+                count = 1;
+            } else if (arr.get(i) == max) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
