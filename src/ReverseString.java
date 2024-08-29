a1ffd0ebc72ae6d8addf36ea2c929f3ec4a5caac
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-08-29 19:53:31 +0530

    reverse string

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,2 +1,16 @@
 public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello";
+        char[] charArray = str.toCharArray();
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.println(new String(charArray));
+    }
 }
+
